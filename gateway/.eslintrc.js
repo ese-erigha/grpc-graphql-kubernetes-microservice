0015a8c6b118ed6245b31da6a8c4853aac18354c
Eseoghene Erigha <ese.erigha@gmail.com>
2022-02-04 02:17:00 +0100

    gateway package setup

---
+++ gateway/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 2, // Means error
+    'no-unused-vars': 'off',
+    'import/extensions': 'off',
+    'class-methods-use-this': 'off'
+  }
+};
