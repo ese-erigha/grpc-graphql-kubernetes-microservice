e8b6910662d8b304aa2f6082ef16feee2f709f3e
Eseoghene Erigha <ese.erigha@gmail.com>
2022-02-13 00:31:12 +0100

    Add app initialization command

--- microservices/post-service/main.py
+++ microservices/post-service/main.py
@@ -18,6 +18,8 @@ app.include_router(routes)
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9002)
 
+# python3 main.py
+
 # https://fastapi.tiangolo.com/uk/deployment/docker/#build-a-docker-image-for-fastapi
 # Start with the url below because it conforms the service project folder
 # https://fastapi.tiangolo.com/uk/deployment/docker/#build-a-docker-image-with-a-single-file-fastapi
