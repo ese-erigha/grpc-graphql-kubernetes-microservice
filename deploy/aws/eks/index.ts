6490853e4da519c3a0af7b2c5a1410feed18e07a
Eseoghene Erigha <ese.erigha@gmail.com>
2022-04-25 12:06:37 +0100

    Create VPC with CIDR and Subnets

--- deploy/aws/eks/index.ts
+++ deploy/aws/eks/index.ts
@@ -4,11 +4,34 @@ import * as eks from '@pulumi/eks';
 import * as config from '../../config';
 
 const projectName = pulumi.getProject();
+const clusterName = `${projectName}-cluster`;
+const clusterTag = `kubernetes.io/cluster/${clusterName}`;
 
-const createVPC = () => new awsx.ec2.Vpc(`${projectName}-vpc-alb`, {});
-export const createCluster = () => {
-  const vpc = createVPC();
-  return new eks.Cluster(`${projectName}-cluster`, {
+export const createVpc = () =>
+  new awsx.ec2.Vpc(`${projectName}-vpc-alb`, {
+    cidrBlock: '172.16.0.0/24',
+    subnets: [
+      {
+        type: 'private',
+        tags: {
+          [clusterTag]: 'owned',
+          'kubernetes.io/role/internal-elb': '1'
+        }
+      },
+      {
+        type: 'public',
+        tags: {
+          [clusterTag]: 'owned',
+          'kubernetes.io/role/elb': '1'
+        }
+      }
+    ],
+    tags: {
+      Name: `${projectName}-vpc`
+    }
+  });
+export const createCluster = (vpc: awsx.ec2.Vpc) => {
+  return new eks.Cluster(clusterName, {
     vpcId: vpc.id,
     enabledClusterLogTypes: ['api', 'audit', 'controllerManager'],
     publicSubnetIds: vpc.publicSubnetIds,
