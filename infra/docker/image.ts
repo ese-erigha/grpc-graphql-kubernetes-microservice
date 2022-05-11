3c0f47186f941d1941e7ef83b8cadaa7b23fbccd
Eseoghene Erigha <ese.erigha@gmail.com>
2022-05-11 02:59:33 +0100

    nits: use pulumi interpolation

--- infra/docker/image.ts
+++ infra/docker/image.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as docker from '@pulumi/docker';
+import * as pulumi from '@pulumi/pulumi';
 import { ImageInput, ImageType } from '../types';
 
 const buildContext = (name: string, type: ImageType) => {
@@ -12,13 +13,13 @@ const buildContext = (name: string, type: ImageType) => {
 
 export const buildImage = (input: ImageInput) => {
   const { stack, timestamp, name, type, repo } = input;
-  const repoUrl = repo.repositoryUrl.apply((url) => url);
   const context = buildContext(name, type);
   return new docker.Image(`${name}`, {
     build: {
       context,
       extraOptions: ['--quiet'] // Fixes https://github.com/pulumi/pulumi-docker/issues/289
     },
-    imageName: `${repoUrl}:${name}-${stack}-${timestamp}`
+    imageName: pulumi.interpolate`${repo.repositoryUrl}:${name}-${stack}-${timestamp}`,
+    localImageName: `${name}-${stack}-${timestamp}`
   });
 };
